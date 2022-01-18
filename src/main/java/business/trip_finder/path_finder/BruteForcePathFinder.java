58030a23f8049978184b5f0afe0fd3f6c088a6c7
aladd <aladdineben@outlook.com>
2022-01-18 10:49:15 +0100

    Implementing the bruteforce algorithm to get the shortest path.

--- src/main/java/business/trip_finder/path_finder/BruteForcePathFinder.java
+++ src/main/java/business/trip_finder/path_finder/BruteForcePathFinder.java
@@ -1,12 +1,63 @@
 package business.trip_finder.path_finder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.paukov.combinatorics3.Generator;
+
+import business.trip.places.Site;
 
 public class BruteForcePathFinder implements PathFinder {
-
+	
 	@Override
 	public PlacesPath findPath(PlacesInput placesInput) {
-		return new PlacesPath(null, List.of(), null);
+		Map<Double, List<Site>> possibleTrips = new HashMap<Double, List<Site>>();
+		Double totalDistance = 0.0;
+		
+		List<Site> sites = placesInput.getSites();
+		
+		/* 
+		 * Simple permutations of the list of sites making every combinations of sites possible
+		 * without repetitions. For more info, check the combinatoricsLib3 on github.
+		 * */
+		List<List<Site>> collectedSites = Generator.permutation(sites)
+		  .simple()
+		  .stream()
+		  .collect(Collectors.toList());
+		
+		for(List<Site> sites2: collectedSites) {
+			if(sites2.size() > 1) {
+				totalDistance += placesInput.getDepartureHotel().getPosition().computeDistance(sites2.get(0).getPosition());
+				for(int index = 0; index < sites2.size()-1; index++) {
+					totalDistance += sites2.get(index).getPosition().computeDistance(sites2.get(index+1).getPosition());
+				}
+				totalDistance += sites2.get(sites2.size()-1).getPosition().computeDistance(placesInput.getArrivalHotel().getPosition());
+				possibleTrips.put(totalDistance, sites2);
+			}
+			else {
+				totalDistance += placesInput.getDepartureHotel().getPosition().computeDistance(sites2.get(0).getPosition());
+				totalDistance += sites2.get(0).getPosition().computeDistance(placesInput.getArrivalHotel().getPosition());
+				possibleTrips.put(totalDistance, sites2);
+			}
+			totalDistance = 0.0;
+		}
+		
+		for(Double value: possibleTrips.keySet()) {
+			System.out.println(value + " : " + possibleTrips.get(value) + "\n");
+		}
+		return new PlacesPath(placesInput.getDepartureHotel(), getLowestKeyMap(possibleTrips), placesInput.getArrivalHotel());
 	}
-
+	
+	private List<Site> getLowestKeyMap(Map<Double, List<Site>> map){
+		Double lowestValue = Double.POSITIVE_INFINITY;
+		for(Double value: map.keySet()) {
+			if(value < lowestValue) {
+				lowestValue = value;
+			}
+		}
+		System.err.println("Returned : " + map.get(lowestValue) + "\n");
+		return map.get(lowestValue);
+	}
 }
